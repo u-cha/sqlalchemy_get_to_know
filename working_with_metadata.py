a7f9453e7c42ee227803cbe3ad4e2bc18c7ba3af
u-cha <s.udachin@mail.ru>
2023-05-22 16:28:42 +0400

    working_with_metadata.py >> creating and dropping tables, invoking explicitly the Table class

---
+++ working_with_metadata.py
@@ -0,0 +1,37 @@
+from sqlalchemy import MetaData
+from sqlalchemy import Table, Column, Integer, String
+from sqlalchemy import ForeignKey
+from sqlalchemy import create_engine
+
+engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+metadata_obj = MetaData()
+
+address_table = Table(
+    'address',
+    metadata_obj,
+    Column('id', Integer, primary_key=True),
+    Column('user_id', ForeignKey('user_account.id'), nullable=False),
+    Column('email_address', String, nullable=False)
+)
+
+user_table = Table(
+    "user_account",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("name", String(30)),
+    Column("fullname", String())
+)
+
+
+
+print(user_table.c.keys())
+print(user_table.primary_key)
+
+print(address_table.c.values())
+print(address_table.description)
+
+metadata_obj.create_all(engine)
+metadata_obj.drop_all(engine)
+
+
+
